acc05f1a34a6eb36a2a80a670c3f9dcb0e4dc485
AntonChetveryk <chetverykanton92@gmail.com>
2020-06-24 21:04:09 +0300

    Updates

---
+++ precache-manifest.d1bbf94c50f9acc6edcb74baa958398a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d80041caff2cd7cdef5fe4f14d1a805",
+    "url": "/react-forms/index.html"
+  },
+  {
+    "revision": "9ec13ca3d964aa8075b3",
+    "url": "/react-forms/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "73a4ab1c7cda61b1185d",
+    "url": "/react-forms/static/css/main.4481c4d7.chunk.css"
+  },
+  {
+    "revision": "9ec13ca3d964aa8075b3",
+    "url": "/react-forms/static/js/2.4e85d8a4.chunk.js"
+  },
+  {
+    "revision": "73a4ab1c7cda61b1185d",
+    "url": "/react-forms/static/js/main.aefcf1c3.chunk.js"
+  },
+  {
+    "revision": "ae477fb449b1a1b38199",
+    "url": "/react-forms/static/js/runtime~main.82238d43.js"
+  }
+]);
